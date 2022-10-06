ab8cb9ce1600c276974acd86e936eea823c6920a
laura.leigh <lauracleigh314@gmail.com>
2022-10-06 13:37:40 +0100

    update get most common letter soec file

--- spec/get_most_common_letter_spec.rb
+++ spec/get_most_common_letter_spec.rb
@@ -1,8 +1,10 @@
 require "get_most_common_letter"
 
 RSpec.describe "get_most_common_letter" do
+
     it "returns o from: the roof, the roof, the roof is on fire!" do
         result = get_most_common_letter("the roof, the roof, the roof is on fire!")
         expect(result).to eq "o"
     end
+    
 end
